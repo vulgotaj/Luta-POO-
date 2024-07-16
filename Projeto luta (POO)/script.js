d67e918c36a210af0a702d100aa2855802681e45
Felipe Tajima <felipebtajima05@gmail.com>
2024-07-16 19:14:31 -0300

    Add files via upload

---
+++ Projeto luta (POO)/script.js
@@ -0,0 +1,14 @@
+let log = new Log(document.querySelector('.log'))
+
+let char = new Knight('Tajima');
+let monster = new BigMonster();
+
+const stage = new Stage(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster'),
+    log
+);
+
+stage.start()
